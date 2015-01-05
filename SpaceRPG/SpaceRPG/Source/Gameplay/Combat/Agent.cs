3bb4b404ceb212186069e0623ad57190a5943378
James Blades Tyson Jr. <linkmaster94@gmail.com>
2015-01-05 15:13:09 -0600

    move to successfully works now

--- SpaceRPG/SpaceRPG/Source/Gameplay/Combat/Agent.cs
+++ SpaceRPG/SpaceRPG/Source/Gameplay/Combat/Agent.cs
@@ -21,7 +21,7 @@ namespace SpaceRPG.Source.Gameplay.Combat
         /// This is the location on a combat grid, not position on a screen
         /// </summary>
         public Vector2 Location, Destination;
-        public bool Moving, Busy, MyTurn;
+        public bool Moving, Busy, MyTurn, Aligned, ReachedNode;
         public List<Point> MovementNodes;
         public delegate void TurnChange();
         [XmlIgnore]
@@ -59,39 +59,84 @@ namespace SpaceRPG.Source.Gameplay.Combat
             {
                 // Get our new position
                 Image.Position += Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                Location = Image.Position / 32;
+                Location = new Vector2((int)Image.Position.X/32, (int)Image.Position.Y/32);
 
                 // Check if we reaced a node
-                if ((int)Location.X == MovementNodes[0].X && (int)Location.Y == MovementNodes[0].Y)
+                if (ReachedNode || (int)Location.X == MovementNodes[0].X && (int)Location.Y == MovementNodes[0].Y)
                 {
-                    MovementNodes.RemoveAt(0);
-                    Console.WriteLine(">> Reached Node <<");
-                    // Check if we reached the last node
-                    if (MovementNodes.Count == 0)
+                    ReachedNode = true;
+                    // Now we need to make sure that our image is moving to align with the top left corner of the square
+                    if (Velocity.X > 0) // moving to the right, so we may need to hop back left
                     {
-                        Velocity = Vector2.Zero;
-                        Moving = false;
-                        Busy = false;
-                        Console.WriteLine(">>> Movement complete <<<");
-                        TurnIsOver();
+                        if (Image.Position.X >= MovementNodes[0].X * 32)
+                        {
+                            Image.Position.X = MovementNodes[0].X * 32;
+                            Aligned = true;
+                        }
                     }
-                    else
-                    {
-                        // Calculate the velocity
-                        Vector2 diff = new Vector2(MovementNodes[0].X - (int)Location.X, MovementNodes[0].Y - (int)Location.Y);
-                        if (diff.X > 0)
-                            Velocity.X = MoveSpeed;
-                        else if (diff.X < 0)
-                            Velocity.X = -MoveSpeed;
-                        else
-                            Velocity.X = 0;
 
-                        if (diff.Y > 0)
-                            Velocity.Y = MoveSpeed;
-                        else if (diff.Y < 0)
-                            Velocity.Y = -MoveSpeed;
+                    else if (Velocity.X < 0) // moving to the left, so we may need to hop back right
+                    { 
+                        if (Image.Position.X <= MovementNodes[0].X * 32)
+                        {
+                            Image.Position.X = MovementNodes[0].X * 32;
+                            Aligned = true;
+                        }
+                    }
+
+                    else if (Velocity.Y > 0) // moving up, so we may need to hop back down
+                    {
+                        if (Image.Position.Y >= MovementNodes[0].Y * 32)
+                        {
+                            Image.Position.Y = MovementNodes[0].Y * 32;
+                            Aligned = true;
+                        }
+                    }
+                    
+                    else if (Velocity.Y < 0) // moving up, so we may need to hop back down
+                    {
+                        if (Image.Position.Y <= MovementNodes[0].Y * 32)
+                        {
+                            Image.Position.Y = MovementNodes[0].Y * 32;
+                            Aligned = true;
+                        }
+                    }
+
+                    if (Aligned)
+                    {
+                        MovementNodes.RemoveAt(0);
+                        Console.WriteLine(">> Reached Node <<");
+
+                        // Check if we reached the last node
+                        if (MovementNodes.Count == 0)
+                        {
+                            Velocity = Vector2.Zero;
+                            Moving = false;
+                            Busy = false;
+                            Console.WriteLine(">>> Movement complete <<<");
+                            TurnIsOver();
+                        }
                         else
-                            Velocity.Y = 0;
+                        {
+                            // Calculate the velocity
+                            Location = new Vector2((int)Image.Position.X / 32, (int)Image.Position.Y / 32);
+                            Vector2 diff = new Vector2(MovementNodes[0].X - (int)Location.X, MovementNodes[0].Y - (int)Location.Y);
+                            if (diff.X > 0)
+                                Velocity.X = MoveSpeed;
+                            else if (diff.X < 0)
+                                Velocity.X = -MoveSpeed;
+                            else
+                                Velocity.X = 0;
+
+                            if (diff.Y > 0)
+                                Velocity.Y = MoveSpeed;
+                            else if (diff.Y < 0)
+                                Velocity.Y = -MoveSpeed;
+                            else
+                                Velocity.Y = 0;     
+                        }
+                        Aligned = false;
+                        ReachedNode = false;
                     }
                 }
             }
@@ -115,17 +160,42 @@ namespace SpaceRPG.Source.Gameplay.Combat
             Destination = newLoc;
             Moving = true;
             Busy = true;
-            
-            // Add our starting location as the first node
-            Point node1 = new Point((int)Location.X, (int)Location.Y);
-            MovementNodes.Add(node1);
-            // Find the nodes to get there in two straight lines (north/south, east/west)
-            Point node2 = new Point((int)newLoc.X, (int)Location.Y);
-            Point node3 = new Point(node2.X, (int)newLoc.Y);
-            MovementNodes.Add(node2);
-            MovementNodes.Add(node3);
+            Aligned = false;
 
-            Console.WriteLine("Moving to point {0} through nodes {1}, {2}, {3}.", node3.ToString(), node1.ToString(), node2.ToString(), node3.ToString());
+            // Snap ouselves to the grid...bandaid fix this later
+            Location = new Vector2((int)Image.Position.X / 32, (int)Image.Position.Y / 32);
+
+            // Add our starting location as the first node
+
+            // Find the horizontal node to match x positions and then move to the end location
+            if (newLoc.X != Location.X)
+            {
+                Point horNode = new Point((int)newLoc.X, (int)Location.Y);
+                MovementNodes.Add(horNode);
+            }
+            if (newLoc.Y != Location.Y)
+            {
+                Point endNode = new Point((int)newLoc.X, (int)newLoc.Y);
+                MovementNodes.Add(endNode);
+            }
+
+            // Calculate the velocity
+            Vector2 diff = new Vector2(MovementNodes[0].X - (int)Location.X, MovementNodes[0].Y - (int)Location.Y);
+            if (diff.X > 0)
+                Velocity.X = MoveSpeed;
+            else if (diff.X < 0)
+                Velocity.X = -MoveSpeed;
+            else
+                Velocity.X = 0;
+
+            if (diff.Y > 0)
+                Velocity.Y = MoveSpeed;
+            else if (diff.Y < 0)
+                Velocity.Y = -MoveSpeed;
+            else
+                Velocity.Y = 0;
+
+            //Console.WriteLine("Moving to point {0} through nodes {1}, {2}, {3}.", vertNode.ToString(), node1.ToString(), horNode.ToString(), vertNode.ToString());
         }
     }
 }
